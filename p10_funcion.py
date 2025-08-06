bda497a57cd2489307e686336a5adbe71951ee6d
Your Name <you@example.com>
2025-08-06 13:11:06 -0600

    mi primer commit

--- p10_funcion.py
+++ p10_funcion.py
@@ -1,8 +1,10 @@
-#Ejemplo de aplicacion y creacion de funciones primero creada y despues se llama 
+#Ejemplo de aplicacion y creacion de funciones primero creada y despues se aplica 
 
-def suma(a,b):
-    return a+b
+'''def suma(a,b):
+    return a+b'''
+#aqui estoy llamando a la funcion pero de manera externa en otro archivo llamado mylibreria
+import mylibreria 
 
 if __name__ == "__main__":
-    print("resutaldo: ", suma(5,6))
-    print("resutaldo: ", suma('5','6'))
+    print("resutaldo: ", mylibreria.suma(5,6))
+    print("resutaldo: ", mylibreria.suma('5','6'))
